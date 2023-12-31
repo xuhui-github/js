2786292535611475eaac6a229b8b71a1e48ce86b
Paul "Moo" Cowboy <john.doe@example.net>
2023-12-31 12:28:15 +0800

    first commit

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,13 +1,20 @@
+
+
+
 var mysql=require('mysql');
+var mongoose=require('mongoose');
+
+
 var con=mysql.createConnection({host: 'localhost',user: 'xuhui',password: 'flower'});
 con.connect(function(err){
 if(err){
-  console.err('error connection: '+err.stack);
-  return;
-  }
-  console.log('connected as id ' + con.threadId);
+    console.err('error connection: '+err.stack);
+    
+
+    return;
+}
+    console.log('connected as id ' + con.threadId);
+
+
 });
-var mongodb=require('mongodb');
-
-
 
